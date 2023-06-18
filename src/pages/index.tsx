c3cfabc3bf58b89ea1218694066b431092898190
Watanabe, Miki <miki.watanabe@hsrw.org>
2023-06-18 19:33:33 +0200

    add directories: pages and blog

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import Layout from '../app/layout';
+
+const HomePage = () => {
+  return (
+    <Layout>
+      <div>
+        Welcome to the Home Page
+      </div>
+    </Layout>
+  );
+};
+
+export default HomePage;
