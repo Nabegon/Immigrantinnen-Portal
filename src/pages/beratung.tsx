2effc2393cc67b805f60602a06a441f48c37fb85
Watanabe, Miki <miki.watanabe@hsrw.org>
2023-07-24 22:17:56 +0200

    add(contents): frauenrechte and beratung

--- src/pages/beratung.tsx
+++ src/pages/beratung.tsx
@@ -4,29 +4,141 @@ const Beratung = () => {
   return (
     <div className="h-screen font-Montserrat flex flex-col">
       <main className="flex-1">
-      <h1 className="text-6xl font-bold text-center">Beratungshilfe für Migrantinnen</h1>
-      <p  className="text-3xl font-bold text-center mt-4 mb-4 text-violet">Überblick über das Thema</p>
-      <p style={{ wordBreak: 'break-all' }}>TextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextText</p>
+        <h1 className="md:text-6xl text-3xl font-bold text-center">Beratungshilfe für Migrantinnen</h1>
+        <p  className="md:text-3xl text-1xl font-bold text-center mt-4 mb-4 text-violet">Überblick über das Thema</p>
+        <p className="md:text-base text-sm">
+          Das Leben im Ausland als Migrantin kann eine aufregende und herausfordernde Erfahrung sein. Es eröffnet zwar neue 
+          Möglichkeiten und kulturelle Erfahrungen, bringt aber auch einzigartige Belastungen und emotionale Herausforderungen mit sich. 
+          Viele Menschen haben in unterschiedlichem Maße die Lasten erlebt, die mit dem Leben in einem fremden Land und den möglichen Auswirkungen auf 
+          das psychische Wohlbefinden einhergehen. Es ist jedoch wichtig, die Gründe für diese emotionalen Belastungen zu verstehen und zu erkennen, 
+          wie wichtig es ist, als Migrantin psychologische Beratung in Anspruch zu nehmen.
+        </p>
         <div className="grid grid-cols-10 gap-2 flex justify-start items-end">
-          <div className="bg-container-gray mr-2 col-span-10 row-start-6 flex flex-col justify-between items-start h-64">
-            <p className="text-4xl font-bold text-center">Beratungsstellen</p>
-            </div>
+          <div className="bg-container-gray mr-2 col-span-10 row-start-6 flex flex-col md:text-start text-center max-h-64 min-h-64">
+            <p className="md:text-4xl text-3xl font-bold md:text-start text-center m-3">Beratungsstellen</p>
+            <p className="md:text-base text-sm max-h-96 overflow-y-scroll m-3">
+              Es gibt Beratungsstellen und Kliniken in ganz Deutschland, die psychische Unterstützung bieten. 
+              Diese Stellen bieten oft eine Vielzahl von Dienstleistungen an, wie zum Beispiel Einzelberatung, 
+              Krisenintervention und Selbsthilfegruppen. Sie können sich auf bestimmte Bereiche spezialisieren, 
+              wie Trauma, Sucht oder Angststörungen. Wenn du Informationen über öffentliche Beratungsstellen in 
+              deiner Stadt erfahren möchtest, suche nach Psychosozialen Beratungsstellen. 
+              Dort findest du Informationen über Beratungsstellen und öffentliche Unterstützung im Bereich 
+              psychische Gesundheit in deiner Stadt. Beratungssitzungen in Beratungsstellen sind normalerweise 
+              kostenlos oder kostengünstig.
+            </p>
+          </div>
         </div>
         <div className="grid grid-cols-10 gap-2 flex justify-start items-end">
-          <div className="bg-container-gray mr-2 col-span-10 row-start-6 flex flex-col justify-between items-start h-64">
-            <p className="text-4xl font-bold text-center">Psychiartrische Kliniken</p>
-            </div>
+          <div className="bg-container-gray mr-2 col-span-10 row-start-6 flex flex-col md:text-start text-center max-h-64 min-h-64">
+            <p className="md:text-4xl text-3xl font-bold md:text-start text-center m-3">Krankenhäuser und Kliniken</p>
+              <p className="md:text-base text-sm max-h-96 overflow-y-scroll m-3">
+                Psychotherapie und psychiatrische Krankenhäuser und Kliniken sind zwei wichtige Formen der psychischen Unterstützung in Deutschland.
+                Bei der Psychotherapie sprichst du mit einer ausgebildeten Fachkraft wie einem Psychologen oder Psychotherapeuten, der dir dabei 
+                helfen kann, verschiedene psychische Probleme anzusprechen und zu bewältigen. Sie kann in verschiedenen Formaten angeboten werden, 
+                einschließlich Einzeltherapie, Gruppentherapie oder Familientherapie.
+                Für Menschen mit schweren psychischen Erkrankungen bieten psychiatrische Krankenhäuser und Kliniken spezialisierte Versorgung und 
+                Behandlung an. Diese Einrichtungen verfügen über multidisziplinäre Teams, bestehend aus Psychiatern, Psychologen, Krankenschwestern und 
+                anderen Fachkräften im Bereich psychische Gesundheit, die zusammenarbeiten, um psychische Störungen zu beurteilen, zu diagnostizieren und zu behandeln.
+                Es ist wichtig zu beachten, dass Psychotherapie in der Regel leichter zugänglich ist, während psychiatrische Krankenhäuser und Kliniken hauptsächlich 
+                Menschen mit schweren Erkrankungen behandeln, die eine intensive Betreuung erfordern.
+                Wenn du psychotherapeutische Unterstützung oder spezialisierte Behandlung benötigst, wird empfohlen, dich an deinen örtlichen Gesundheitsdienstleister zu wenden oder in Online-Verzeichnissen nach lizenzierten Fachkräften oder Einrichtungen in deiner Nähe zu suchen. Sie können weitere Informationen über die angebotenen Leistungen, die Verfügbarkeit und mögliche mehrsprachige Unterstützung geben. Zusätzlich können Organisationen wie die Deutsche Psychotherapeuten Vereinigung und die Kassenärztliche Vereinigung bei der Suche nach geeigneten Ressourcen für deine spezifischen Bedürfnisse hilfreich sein.
+              </p>
+          </div>
         </div>
-        <div className="grid grid-cols-10 gap-2 flex justify-start items-end">
-          <div className="bg-container-gray mr-2 col-span-10 row-start-6 flex flex-col justify-between items-start h-64">
-            <p className="text-4xl font-bold text-center">Psychotherapie</p>
-            </div>
+        <div className="grid grid-cols-10 gap-2 flex justify-start items-end mb-5">
+          <div className="bg-container-gray mr-2 col-span-10 row-start-6 flex flex-col md:text-start text-center max-h-64">
+            <p className="md:text-4xl text-3xl font-bold md:text-start text-center m-3">Psychotherapie</p>
+              <p className="md:text-base text-sm max-h-96 overflow-y-scroll m-3">
+                Wenn du nicht sicher bist, ob du eine Psychotherapie benötigst, kannst du dich zuerst an deinen Hausarzt, 
+                eine psychologische Beratungsstelle, eine psychiatrische oder psychotherapeutische Praxis wenden, 
+                um erste Ratschläge zu erhalten. Bei psychischen Erkrankungen wie Depressionen, Angststörungen oder 
+                Essstörungen übernimmt die gesetzliche Krankenversicherung die Kosten für psychotherapeutische 
+                Behandlungen, die von zugelassenen Psychotherapeuten oder Ärzten empfohlen werden. In der Regel 
+                musst du bei Abwesenheit einer psychischen Erkrankung die Kosten für die Psychotherapie selbst tragen. 
+                Zu den von der Versicherung erstatteten psychotherapeutischen Ansätzen gehören Verhaltenstherapie, 
+                tiefenpsychologisch fundierte Therapie, Psychoanalyse und andere Formen der Psychotherapie. 
+                Für konkrete Verfahren und den Umfang der Leistungen empfehle ich dir, deine Krankenversicherung zu 
+                kontaktieren und dies zu klären. Wenn festgestellt wurde, dass eine psychotherapeutische Behandlung 
+                notwendig ist, musst du auch deine Krankenversicherung informieren.
+              </p>
+          </div>
         </div>
-        <div className="grid grid-cols-10 gap-2 flex justify-start items-end">
-          <div className="bg-container-gray mr-2 col-span-10 row-start-6 flex flex-col justify-between items-start h-64">
-            <p className="text-4xl font-bold text-center">Helplines und Hotlines</p>
-            </div>
+        <div className="grid grid-cols-10 gap-2 flex justify-start items-end mb-5">
+          <div className="bg-container-gray mr-2 col-span-10 row-start-6 flex flex-col md:text-start text-center max-h-64">
+            <p className="md:text-4xl text-3xl font-bold md:text-start text-center m-3">Helplines und Hotlines</p>
+              <p className="md:text-base text-sm max-h-96 overflow-y-scroll m-3">
+                In Deutschland stehen dir verschiedene Helplines und Hotlines zur Verfügung, um sofortige Unterstützung 
+                bei psychischen Problemen zu erhalten. Diese Dienste bieten vertrauliche und anonyme Hilfe, damit du 
+                über deine Sorgen sprechen, Rat erhalten und auf Ressourcen zugreifen kannst.
+              </p>
+          </div>
         </div>
+        <p  className="md:text-3xl text-1xl font-bold text-center mt-8 mb-4 text-violet">Kontaktinformationen</p>
+        <ul className="md:text-base text-sm">
+        <li className="my-3">
+            <strong>・ Telefonseelsorge</strong>
+            <br />
+            Telefonseelsorge is a nationwide helpline available 24/7 that offers emotional support and counseling. It is operated by trained volunteers who provide a listening ear and guidance. The service is confidential and anonymous, allowing individuals to freely discuss their concerns.
+            <br />
+            <strong>Hotline:</strong> 0800 111 0 111 (for emotional support)
+            <br />
+            <strong>Hotline:</strong> 0800 111 0 222 (for counseling in crisis situations)
+          </li>
+          <li className="my-3">
+            <strong>・ Nummer gegen Kummer</strong>
+            <br />
+            Nummer gegen Kummer is a helpline specifically dedicated to children, teenagers, and their parents. It offers professional counseling and support on a range of topics, including mental health, family issues, bullying, and more. The service is available in multiple languages.
+            <br />
+            <strong>Hotline:</strong> 116 111 (for children and teenagers)
+            <br />
+            <strong>Hotline:</strong> 116 123 (for parents)
+          </li>
+          <li className="my-3">
+            <strong>・ Hilfetelefon Gewalt gegen Frauen</strong>
+            <br />
+            Hilfetelefon Gewalt gegen Frauen is a helpline specifically for women who are experiencing or have experienced violence. It provides support, information, and counseling on issues related to domestic violence, sexual assault, and abuse. The service is available 24/7 and is confidential.
+            <br />
+            <strong>Hotline:</strong> 08000 116 016
+          </li>
+          <li className="my-3">
+            <strong>・ Deutsche Depressionshilfe</strong>
+            <br />
+            Deutsche Depressionshilfe is an organization that offers a helpline for individuals affected by depression. Trained professionals provide information, support, and guidance for those struggling with depression or supporting someone with depression. The helpline operates on weekdays from 8 am to 8 pm.
+            <br />
+            <strong>Hotline:</strong> 0800 334 4533
+          </li>
+          <li className="my-3">
+            <strong>・ Türkischer Bund in Berlin-Brandenburg (Turkish Association in Berlin-Brandenburg)</strong>
+            <br />
+            Support for Turkish-speaking individuals
+            <br />
+            Website: <a href="https://www.tbb-berlin.de">www.tbb-berlin.de</a>
+            <br />
+            <strong>Hotline:</strong> +49 30 4423233 (Monday to Friday, 10 am to 6 pm)
+          </li>
+          <li className="my-3">
+            <strong>・ Immigrant Women's Health and Counseling Center (FIM)</strong>
+            <br />
+            Counseling and support for immigrant women
+            <br />
+            Website: <a href="https://www.fim-frauenrecht.de">www.fim-frauenrecht.de</a>
+            <br />
+            <strong>Hotline:</strong> +49 30 3929888 (Monday to Friday, 10 am to 6 pm)
+            <br />
+            Languages: German, English, Arabic, and Turkish
+          </li>
+          <li className="my-3">
+            <strong>・ Telefonseelsorge (Telephone Counseling)</strong>
+            <br />
+            Emotional support and crisis intervention helpline
+            <br />
+            Website: <a href="https://www.telefonseelsorge.de">www.telefonseelsorge.de</a>
+            <br />
+            <strong>Hotline:</strong> 0800 111 0 111 or 0800 111 0 222 (available 24/7)
+            <br />
+            Languages: German, English, and other languages through interpreters
+          </li>
+        </ul>
       </main>
       <Footer />
     </div>
--- src/pages/frauenrechte.tsx
+++ src/pages/frauenrechte.tsx
@@ -7,23 +7,57 @@ const Frauenrechte = () => {
   return (
     <div className="h-screen font-Montserrat flex flex-col">
       <main className="flex-1">
-      <h1 className="text-6xl font-bold text-center">Ihre Rechte <br></br>als Migrantinnen in Deutschland</h1>
-      <p  className="text-3xl font-bold text-center mt-4 mb-4 text-violet">Überblick über das Thema</p>
-      <p style={{ wordBreak: 'break-all' }}>TextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextText</p>
+      <h1 className="md:text-6xl text-3xl font-bold text-center">Ihre Rechte <br></br>als Migrantinnen in Deutschland</h1>
+      <p  className="md:text-3xl text-1xl font-bold text-center mt-4 mb-4 text-violet">Überblick über das Thema</p>
+      <p className="md:text-base text-sm">
+        Frauenrechte und Gleichstellung waren schon immer eines der 
+        wichtigsten Themen hier in Deutschland. Das Land setzt sich dafür ein, deine Rechte als Frau zu 
+        fördern und zu verteidigen und jegliche Form von Diskriminierung und Gewalt gegen Frauen zu 
+        bekämpfen. Trotzdem ist es eine Tatsache, dass Diskriminierung gegen Frauen in allen Bereichen 
+        existiert. Auch Imigrantinnen können manchmal schnell in eine sozial vulnerable Situation geraten. 
+        Deshalb ist es für dich als Imigrantin, die hier in Deutschland lebt, sehr wichtig, deine Rechte zu 
+        verstehen und zu wissen, welche Ressourcen dir zur Verfügung stehen. Auf dieser Seite findest du 
+        einen Überblick über Frauenrechte in Deutschland.
+      </p>
         <div className="grid grid-cols-10 gap-2 flex justify-start items-end">
-          <div className="bg-container-gray mr-2 col-span-10 row-start-6 flex flex-col justify-between items-start h-64">
-            <p className="text-4xl font-bold text-center">Rechtlicher Schutz</p>
+          <div className="bg-container-gray mr-2 col-span-10 row-start-6 flex flex-col md:text-start text-center max-h-64">
+            <p className="md:text-4xl text-3xl font-bold md:text-start text-center m-3">Rechtlicher Schutz</p>
+            <p className="md:text-base text-sm max-h-96 overflow-y-scroll m-3">
+              In Deutschland hat sich starke Gesetze, die dich als Frau schützen. 
+              Der Grundgesetz stellt sicher, dass alle Menschen, egal welches Geschlecht sie haben, 
+              die gleichen Rechte haben. Das bedeutet, dass wir Frauen in vielen Bereichen gleich 
+              behandeln, sei es bei der Arbeit, in der Schule oder bei öffentlichen Dienstleistungen. 
+              Wir haben Gesetze, die vor Diskriminierung schützen und sicherstellen, 
+              dass du als Frau fair behandelt wirst. Zum Beispiel müssen wir dasselbe Gehalt wie Männer 
+              bekommen, wir dürfen nicht aufgrund unseres Geschlechts benachteiligt werden und es gibt 
+              auch Regelungen für unseren Mutterschaftsurlaub.
+            </p>
             </div>
         </div>
         <div className="grid grid-cols-10 gap-2 flex justify-start items-end">
-          <div className="bg-container-gray mr-2 col-span-10 row-start-6 flex flex-col justify-between items-start h-64">
-            <p className="text-4xl font-bold text-center">Rechtlicher Schutz</p>
-            </div>
+          <div className="bg-container-gray mr-2 col-span-10 row-start-6 flex flex-col md:text-start text-center max-h-64">
+            <p className="md:text-4xl text-3xl font-bold md:text-start text-center m-3">Gleichberechtigung</p>
+              <p className="md:text-base text-sm max-h-96 overflow-y-scroll m-3">
+                In Deutschland setzen wir uns aktiv für die Gleichberechtigung von Frauen und Männern in allen Lebensbereichen 
+                ein. Wir ergreifen Maßnahmen, um sicherzustellen, dass du als Frau gleiche Chancen und Vertretung in der Politik, 
+                in Führungspositionen und am Arbeitsplatz hast. Wir fördern eine ausgewogene Work-Life-Balance durch flexible 
+                Arbeitszeiten, Möglichkeiten für Elternzeit und bezahlbare Kinderbetreuung. Darüber hinaus haben wir Initiativen, 
+                die Geschlechterklischees herausfordern und eine geschlechtssensible Sprache fördern, um eine inklusive Umgebung für 
+                uns alle zu schaffen.
+              </p>
+          </div>
         </div>
-        <div className="grid grid-cols-10 gap-2 flex justify-start items-end">
-          <div className="bg-container-gray mr-2 col-span-10 row-start-6 flex flex-col justify-between items-start h-64">
-            <p className="text-4xl font-bold text-center">Rechtlicher Schutz</p>
-            </div>
+        <div className="grid grid-cols-10 gap-2 flex justify-start items-end mb-5">
+          <div className="bg-container-gray mr-2 col-span-10 row-start-6 flex flex-col md:text-start text-center max-h-64">
+            <p className="md:text-4xl text-3xl font-bold md:text-start m-3">Schutz vor Gewalt</p>
+              <p className="md:text-base text-sm max-h-96 overflow-y-scroll m-3">
+                Deutschland nimmt eine klare Haltung in Bezug auf Gewalt gegen Frauen ein. Es gibt Gesetze und Hilfsprogramme, 
+                um dich vor häuslicher Gewalt, sexuellem Missbrauch und Belästigung zu schützen. Wenn du dich in einer 
+                Situation befindest, in der du Gewalt oder Missbrauch erlebst, ist es wichtig zu wissen, dass Hilfe verfügbar 
+                ist. Suche Unterstützung bei örtlichen Frauenhäusern, Beratungsstellen und Notrufnummern. Diese Organisationen 
+                bieten dir vertrauliche Unterstützung, Rat und einen sicheren Ort, wenn du schwierige Situationen durchlebst.
+              </p>
+          </div>
         </div>
       </main>
       <Footer />
