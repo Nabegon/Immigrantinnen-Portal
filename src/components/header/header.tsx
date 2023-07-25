59054dde5f882d48b21c406dff81e18a79fe2c83
Watanabe, Miki <miki.watanabe@hsrw.org>
2023-07-25 16:44:13 +0200

    add logo and a top page image

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -6,7 +6,9 @@ const Header = () => {
     <div className="container">
       <div className="flex flex-col md:flex-row justify-between items-center m-5 font-Montserrat text-xs md:text-lg">
         <Link href="/">
-          <div className="m-5 md:m-0">Logo</div>
+          <div className="w-24 h-24 mt-5">
+            <img src="/logo_immigrantin_portal.svg" alt="Logo" />
+          </div>
         </Link>
 
         <ul className="flex space-x-4 md:space-x-10 font-Montserrat text-xs md:text-lg">
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,14 +10,18 @@ const Home = () => {
         <div className="grid grid-cols-1 md:grid-cols-10 gap-2 flex justify-start items-end">
           <div className="md:col-span-3 md:row-span-6 md:row-start-6 flex flex-col justify-between items-start md:h-64">
             <p className="text-4xl font-bold text-center m-5 md:m-0">Empowerment&nbsp; von Migrantinnen</p>
-            <p className="text-center  m-5 md:m-0">Einfacher Guide zu Rechten, Ressourcen und Möglichkeiten</p>
-            <button className="bg-violet hover:bg-violet-500 text-white text-xl px-4 py-2 w-56 ml-14 m-5 md:m-0">
+            <p className="text-center m-0 md:m-2">Einfacher Guide zu Rechten, Ressourcen und Möglichkeiten</p>
+            <button className="bg-violet hover:bg-violet-500 text-white text-xl px-1 py-2 w-56 m-16">
               <Link href="/frauenrechte">
                 Mehr Infos
               </Link>
             </button>
           </div>
-          <div className="col-span-1 md:col-span-7 md:row-span-6 md:row-start-6 bg-container-gray h-40 md:h-96">02</div>
+          <div className="col-span-1 md:col-span-7 md:row-span-6 md:row-start-6 bg-container-gray h-40 md:h-96 border border-black-500">
+            {/* <div className="w-24 h-24 mt-5"> */}
+              <img src="/top-page-image.svg" alt="Logo" className="w-full h-full" />
+            {/* </div> */}
+          </div>
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
@@ -54,40 +58,6 @@ const Home = () => {
             </button>
           </div>
         </div>
-        {/* <div className="grid grid-cols-3 md:grid-cols-2 gap-4 mt-4">
-          <div className="bg-container-gray row-span-5 flex flex-col justify-between items-start h-96">
-            <p className="self-start text-4xl	font-bold">Frauenrechte</p>
-            <button className="bg-violet hover:bg-violet-500 text-white text-xl px-4 py-2 w-56 mb-2 mx-2">
-              <Link href="/frauenrechte">
-                Mehr wissen
-              </Link>
-            </button>
-          </div>
-          <div className="bg-container-gray row-span-5 flex flex-col justify-between items-start h-96">
-            <p className="self-start text-4xl	font-bold">Beratung</p>
-            <button className="bg-violet hover:bg-violet-500 text-white text-xl px-4 py-2 w-56 mb-2 mx-2">
-              <Link href="/beratung">
-                Mehr wissen
-              </Link>
-            </button>
-          </div>
-          <div className="bg-container-gray row-span-5 flex flex-col justify-between items-start h-96">
-            <p className="self-start text-4xl	font-bold">Job-Hilfe</p>
-            <button className="bg-violet hover:bg-violet-500 text-white text-xl px-4 py-2 w-56 mb-2 mx-2">
-              <Link href="/job-hilfe">
-                Mehr wissen
-              </Link>
-            </button>
-          </div>
-          <div className="bg-container-gray row-span-5 flex flex-col justify-between items-start h-96">
-            <p className="self-start text-4xl	font-bold">Deutsch lernen</p>
-            <button className="bg-violet hover:bg-violet-500 text-white text-xl px-4 py-2 w-56 mb-2 mx-2">
-              <Link href="/deutsch-lernen">
-                Mehr wissen
-              </Link>
-            </button>
-          </div>
-        </div> */}
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4 justify-items-center items-center">
           <div className="bg-container-gray flex justify-center items-center w-full md:w-96 h-40 md:h-96">
             <p className="text-4xl font-bold">Interview 1</p>
